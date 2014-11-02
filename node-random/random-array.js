6f49e7d34ca0e5b16c284f3c005b6524c5023dbd
Rachel Fish <itsrachelfish@gmail.com>
2014-11-02 11:47:33 -0700

    Comparing Math.random() and node crypto

--- node-random/random-array.js
+++ node-random/random-array.js
@@ -1,19 +1,31 @@
 var crypto = require('crypto');
 var array = ['hello', 'world', 'how', 'are', 'you', 'doing', 'today', 'I', 'am', 'fine', 'thanks'];
+var frequency = {'math': {}, 'crypto': {}};
 
-var frequency = {};
 
+// Using Math.random()
+for(var i = 0; i < 10000; i++)
+{
+    var index = Math.floor(Math.random() * array.length);
+    var word = array[index];
+
+    if(typeof frequency.math[index] == "undefined")
+        frequency.math[index] = 0;
+    
+    frequency.math[index]++;
+}
+
+
+// Using node crypto
 for(var i = 0; i < 10000; i++)
 {
     var index = crypto.randomBytes(1).readUInt8(0) % array.length;
     var word = array[index];
 
-    console.log(index, word);
-
-    if(typeof frequency[index] == "undefined")
-        frequency[index] = 0;
+    if(typeof frequency.crypto[index] == "undefined")
+        frequency.crypto[index] = 0;
     
-    frequency[index]++;
+    frequency.crypto[index]++;
 }
 
 console.log(frequency);
