0629216bd8bf24fadb8b8d23158a3f59167e5147
Rachel Shelton <itsrachelfish@gmail.com>
2014-11-10 15:33:22 -0700

    Updated marker generation to use city population data

--- server-map-cluster/generate.js
+++ server-map-cluster/generate.js
@@ -1,59 +1,73 @@
 // Generate random coordinates to be saved in data.js
 
 // Using random locations gives results that are too uniform. We need to use something with more... clusters!
-
-var $ = require('jquery');
+var cheerio = require('cheerio');
+var fs = require('fs');
+var GeoPoint = require('geopoint');
 
 // Get saved HTML data
-var dom = "<body><div>hi lol<b>hey</b></div></body>";
+var $ = cheerio.load(fs.readFileSync('./static/population.html'));
 
-$(dom).find('b').each(function()
+// Remove garbage data
+$('.sortkey').remove();
+$('br').replaceWith('/');
+
+function sanitize(text)
 {
-    console.log($(this));
-});
-
-
-/*
-    Loop through it find:
-    * Population
-    * Land area
-    * Location
-*/
-
-// Loop through each city to generate markers based on population
-// For i = 0; i < population divided by 1000
-// Random jitter = square root of the land area
-// Around the location
-
-/*
-function latlng()
-{
-    // Latitude (-90 to 90)
-    var lat = (Math.floor(Math.random() * 180) - 90) + Math.random();
-
-    // Longitude (-180 to 180)
-    var lng = (Math.floor(Math.random() * 360) - 180) + Math.random();
-
-    return {lat: lat, lng: lng};
+    return text.toString().replace(/[\n\r,]/g, '');
 }
 
 var data = [];
+var markers = [];
 
-for(var i = 0; i < 20000; i++)
+// Loop through each row
+$('tr').each(function()
 {
-    var location = latlng();
+    // Find:
+    // - Population
+    var population = sanitize($(this).find('td').eq(3).text());
 
-    // Only generate locations within North America (provided by https://www.flickr.com/places/info/24865672)
-    if(location.lat > 5.4995 && location.lat < 63.1621 &&
-        location.lng > -147.2764 && location.lng < -52.2330)
+    // - Land area
+    var area = sanitize($(this).find('td').eq(6).text());
+
+    // - Coordinates
+    var coords = sanitize($(this).find('td').eq(8).text());
+
+    if(population && area && coords)
     {
-        data.push(location);
+        // Convert N/W coordinates to lat/lng
+        coords = coords.split(' ');
+
+        var lat = parseFloat(coords[0].toString().replace('°N', ''));
+        var lng = parseFloat(coords[1].toString().replace('°W', '')) * -1;
+
+        // Extract approximate area from text
+        area = area.split('/');
+        area = Math.sqrt(parseFloat(area[0]));
+
+        // Store extracted data
+        data.push({population: population, area: area, location: {lat: lat, lng: lng}});
     }
-    else
+});
+
+// Loop through extracted data
+for(var d_idx = 0, d_l = data.length; d_idx < d_l; d_idx++)
+{
+    var city = data[d_idx];
+    city.point = new GeoPoint(city.location.lat, city.location.lng);
+
+    var bounds = city.point.boundingCoordinates(city.area);
+    var sw = {lat: bounds[0]._degLat, lng: bounds[0]._degLon};
+    var ne = {lat: bounds[1]._degLat, lng: bounds[1]._degLon};
+
+    // Loop to generate markers based on population
+    for(var c_idx = 0, c_l = parseInt(city.population) / 10000; c_idx < c_l; c_idx++)
     {
-        i--;
+        var random_lat = Math.random() * (ne.lat - sw.lat) + sw.lat;
+        var random_lng = Math.random() * (ne.lng - sw.lng) + sw.lng;
+
+        markers.push({lat: random_lat, lng: random_lng});
     }
 }
-*/
 
-console.log("module.exports = "+JSON.stringify(data));
+console.log("module.exports = "+JSON.stringify(markers));
