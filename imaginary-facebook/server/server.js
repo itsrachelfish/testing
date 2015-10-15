9977aa35549210da4605b058dfd8c892b5fdfc20
Rachel Fish <itsrachelfish@gmail.com>
2015-10-15 02:08:22 -0600

    Set up nice urls with browser history API

--- imaginary-facebook/server/server.js
+++ imaginary-facebook/server/server.js
@@ -1,4 +1,5 @@
 // Start the server
+var path = require('path');
 var express = require('express');
 var app = express();
 var http = require('http').createServer(app);
@@ -6,3 +7,10 @@ http.listen(1337);
 
 // Serve static files
 app.use(express.static("../static/"));
+
+// Catchall to handle browser history URLs
+app.get('/*', function (req, res) {
+  res.sendFile(path.resolve(__dirname + "/../static/index.html"));
+});
+
+console.log("Imaginary Facebook Server Started.");
--- imaginary-facebook/static/js/routes.js
+++ imaginary-facebook/static/js/routes.js
@@ -1,16 +1,28 @@
+var createHistory = require('history/lib/createBrowserHistory');
+
 var React = require('react');
 var ReactRouter = require('react-router');
 var Router = ReactRouter.Router;
 var Route = ReactRouter.Route;
 var IndexRoute = ReactRouter.IndexRoute;
 
-var Routes =
-(
-    <Router>
-        <Route name="app" path="/" handler={require('./views/app')}>
+var App = require('./components/app');
+var Home = require('./components/home/homePage');
+var NotFound = require('./components/notFound');
 
-        </Route>
-    </Router>
-);
+var Routes = React.createClass(
+{
+    render: function()
+    {
+        return (
+            <Router history={ createHistory() }>
+                <Route path="/" component={ App }>
+                    <IndexRoute component={ Home } />
+                    <Route path="*" component={ NotFound } />
+                </Route>
+            </Router>
+        );
+    }
+});
 
 module.exports = Routes;
