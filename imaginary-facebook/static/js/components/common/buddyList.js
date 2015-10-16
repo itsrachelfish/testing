b202ba293d6b9fe3fc3affa6d099ad8bcc3c2001
Rachel Fish <itsrachelfish@gmail.com>
2015-10-15 23:56:24 -0600

    Updated buddy list to display status icons

--- imaginary-facebook/static/js/components/common/buddyList.js
+++ imaginary-facebook/static/js/components/common/buddyList.js
@@ -9,26 +9,31 @@ var BuddyList = React.createClass(
                 <div className="friend">
                     <img className="avatar" src="/img/friend-1.png" />
                     <span className="name">Fox McCloud</span>
+                    <span className="status online"></span>
                 </div>
 
                 <div className="friend">
                     <img className="avatar" src="/img/friend-2.png" />
                     <span className="name">Rachel Fish</span>
+                    <span className="status online"></span>
                 </div>
 
                 <div className="friend">
                     <img className="avatar" src="/img/friend-3.jpg" />
                     <span className="name">River God</span>
+                    <span className="status offline"></span>
                 </div>
 
                 <div className="friend">
                     <img className="avatar" src="/img/friend-4.png" />
                     <span className="name">Phantom Fish</span>
+                    <span className="status offline"></span>
                 </div>
 
                 <div className="friend">
                     <img className="avatar" src="/img/friend-5.jpg" />
                     <span className="name">Washing Girl</span>
+                    <span className="status offline"></span>
                 </div>
             </aside>
         );
