ad120e0a044f51f2231f22734867818020c63045
Rachel Fish <itsrachelfish@gmail.com>
2015-11-04 23:01:10 -0700

    Added logging and error handling

--- node-upload/server.js
+++ node-upload/server.js
@@ -4,35 +4,80 @@ var express = require('express');
 var app = express();
 
 app.use(busboy()); 
+app.enable('trust proxy');
+
+function date()
+{
+    return new Date().toLocaleString();
+}
+
+function log()
+{
+    var input = [];
+    for(var i = 0, l = arguments.length; i < l; i++)
+    {
+        input.push(arguments[i]);
+    }
+    
+    input.unshift(date(), ' - ');
+    console.log.apply(this, input);
+}
 
 app.get('/', function (req, res)
 {
+    log('User visited:', req.ip);
     res.send('<form method="post" enctype="multipart/form-data"><input type="file" name="file"><input type="submit" value="Upload"></form>');
 });
 
 // Make sure the uploads directory exists
-fs.mkdir('uploads');
+fs.mkdir('uploads', function(error)
+{
+    if(error)
+    {
+        if(error.code == 'EEXIST')
+        {
+            log('./upload directory already exists!');
+        }
+        else
+        {
+            log('There was an error :O', error);
+        }
+    }
+    else
+    {
+        log('./upload directory created');
+    }
+});
 
-app.post('/', function(req, res) {
+app.post('/', function(req, res)
+{
     var fstream;
     req.pipe(req.busboy);
-    req.busboy.on('file', function (fieldname, file, filename) {
-        console.log("Uploading: " + filename); 
+    req.busboy.on('file', function (fieldname, file, filename)
+    {
+        log("Uploading:" , filename); 
+        log("Uploaded by:", req.ip, '-', req.headers['user-agent']);
+
         fstream = fs.createWriteStream(__dirname + '/uploads/' + filename);
         file.pipe(fstream);
-        fstream.on('close', function () {
+        fstream.on('close', function()
+        {
+            log('File uploaded!');
             res.end('File uploaded!');
+        });
+
+        fstream.on('error', function()
+        {
+            log('There was an error!', arguments);
         });
     });
 });
 
-var server = app.listen(1337, function ()
+var server = app.listen(1337, function()
 {
+    var host = server.address().address
+    var port = server.address().port
 
-  var host = server.address().address
-  var port = server.address().port
-
-  console.log('Example app listening at http://%s:%s', host, port)
-
+    log('Example app listening at http://%s:%s', host, port)
 })
 
