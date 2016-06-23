e753ada24a3dd98cdacaec973ab517c0aeaee64b
Rachel Fish <itsrachelfish@gmail.com>
2016-06-23 08:43:56 -0600

    YESSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSss

--- transform/transform.js
+++ transform/transform.js
@@ -3,12 +3,62 @@ function degrees(radians)
     return radians * (180 / Math.PI);
 }
 
+var line =
+{
+    init: function()
+    {
+        line.canvas = $('canvas').el[0];
+
+        var size = $(line.canvas).size();
+        var position = $(line.canvas).position();
+
+        line.center =
+        {
+            x: size.width / 2,
+            y: size.height / 2
+        };
+
+        line.offset =
+        {
+            x: position.left,
+            y: position.top
+        };
+
+        line.canvas.width = size.width;
+        line.canvas.height = size.height;
+
+        line.context = line.canvas.getContext("2d");
+    },
+
+    draw: function(x, y)
+    {
+        line.refresh();
+
+        x -= line.offset.x;
+        y -= line.offset.y;
+
+        line.context.strokeStyle = 'white';
+        line.context.beginPath();
+        line.context.moveTo(line.center.x, line.center.y);
+        line.context.lineTo(x, y);
+        line.context.closePath();
+        line.context.stroke();
+    },
+
+    refresh: function()
+    {
+        line.context.clearRect(0, 0, line.canvas.width, line.canvas.height);
+    },
+}
+
 var transform =
 {
     active: false,
 
     init: function(selector)
     {
+        line.init();
+
         var template = $('.template').clone();
         $(template).find('.object').el[0].appendChild($(selector).el[0]);
         $(template).removeClass('template');
@@ -26,10 +76,24 @@ var transform =
         transform.active = true;
         transform.element = event.target;
 
+        var size = $(transform.template).size();
         var position = $(transform.template).position();
-        transform.center = {x: position.left, y: position.top};
-        transform.offset = $(this).data('rotate');
 
+        transform.center =
+        {
+            x: size.width / 2,
+            y: size.height / 2
+        };
+
+console.log(size, position);
+
+        transform.offset =
+        {
+            x: position.left,
+            y: position.top
+        };
+
+        transform.rotateOffset = parseInt($(this).data('rotate'));
         $(this).addClass('active');
     },
 
@@ -37,14 +101,29 @@ var transform =
     {
         if(transform.active)
         {
-            var difference =
-            {
-                x: event.clientX - transform.center.x,
-                y: event.clientY - transform.center.y
-            };
+            console.log(transform.center, transform.offset);
 
-            var angle = degrees(Math.atan2(difference.y, difference.x)) + transform.offset;
-            $(transform.template).transform('rotate', Math.atan2(difference.y, difference.x) + 'rad');
+    line.draw(event.clientX, event.clientY);
+/*
+        // Center the box in the middle of the screen
+        x = box.center.x;
+        y = box.center.y;
+
+        // Center the box based on its size
+        x -= box.size / 2;
+        y -= box.size / 2;
+*/
+        // Get the angle of the line relative to the origin
+        var difference =
+        {
+            x: event.clientX - transform.offset.x - transform.center.x,
+            y: event.clientY - transform.offset.y - transform.center.y
+        };
+
+console.log( degrees(Math.atan2(difference.y, difference.x)), transform.rotateOffset);
+
+            var angle = degrees(Math.atan2(difference.y, difference.x)) + transform.rotateOffset;
+            $(transform.template).transform('rotate', angle + 'deg');
         }
     },
 
