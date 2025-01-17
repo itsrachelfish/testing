ea6c4f59fdc527f701d36f9739c4af2cfdcb34c4
Rachel Fish <itsrachelfish@gmail.com>
2025-01-17 03:35:18 -0700

    Who would have guessed parsing json would be so hard

--- go/serial/serial-reader.go
+++ go/serial/serial-reader.go
@@ -2,11 +2,27 @@ package main
 
 import (
 	"go.bug.st/serial"
+    "encoding/json"
 	"log"
 	"fmt"
+    "time"
+    "strings"
 )
 
-func main() {
+type SensorData struct {
+    sensor	string
+    temp   	float32
+    humid 	float32
+}
+
+type SensorOutput struct {
+	timestamp	string
+    sensor		string
+    temp   		float32
+    humid 		float32
+}
+
+func connectSerial() serial.Port {
 	mode := &serial.Mode{
 		BaudRate: 9600,
 	}
@@ -16,13 +32,36 @@ func main() {
 		log.Fatal(err)
 	}
 
-	buffer := make([]byte, 100)
+	return port
+}
+
+func parseSensorData(buffer []byte) SensorOutput {
+	var sensorData SensorData
+
+	err := json.Unmarshal(buffer, &sensorData)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	output := SensorOutput{
+		timestamp: time.Now().String(),
+		sensor:	sensorData.sensor,
+		temp: sensorData.temp,
+		humid: sensorData.humid,
+	}
+	
+	return output
+}
+
+func main() {
+	var port = connectSerial()
+	inputBuffer := make([]byte, 100)
+	lineBuffer := make([]byte, 100)
 
 	for {
-		length, err := port.Read(buffer)
-
-		if err != nil {
-			log.Fatal(err)
+		length, readErr := port.Read(inputBuffer)
+		if readErr != nil {
+			log.Fatal(readErr)
 			break
 		}
 
@@ -31,6 +70,23 @@ func main() {
 			break
 		}
 
-		fmt.Printf("%v", string(buffer[:length]))
+		lineBuffer = append(lineBuffer, inputBuffer...)
+
+		fmt.Println("New input", string(inputBuffer));
+		fmt.Println("Total line", string(lineBuffer));
+		
+		if strings.Contains(string(lineBuffer), "\n") {
+			sensorData := parseSensorData(lineBuffer)
+			lineBuffer = lineBuffer[:0]		
+
+			jsonData, err := json.Marshal(sensorData)
+			if err != nil {
+				fmt.Println("Error:", err)
+				break
+			}
+
+			jsonString := string(jsonData)
+			fmt.Println(jsonString)
+		}
 	}
 }
